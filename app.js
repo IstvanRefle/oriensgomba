0b492bef46b47ca003e2415b5fd163ea8d32b140
Istvan Refle <49591842+IstvanRefle@users.noreply.github.com>
2020-01-20 11:59:14 +0100

    greetings hide

--- app.js
+++ app.js
@@ -8,6 +8,29 @@ function myFunction() {
   }
 }
 
-//function greetingsHide() {
-//	 document.getElementById("txt-greetings");
-//}
+var greetingsDisplay='True';
+var c=1;
+
+function waitForHide() {
+	if (greetingsDisplay=='True') {
+		document.getElementById("txt-greetings").style.display="block";
+		setTimeout(greetingsHide,3000);	
+	}
+}
+
+function greetingsHide() {
+		if (c>=0) {
+		document.getElementById("txt-greetings").style.color="hsla(0, 0%, 95%, " + c + ")";
+		c=c-0.1;
+		if (c>=0) {
+			document.getElementById("txt-greetings").style.backgroundColor="hsla(0, 0%, 0%, " + (c-0.4) + ")";
+		}
+		setTimeout(function() {
+			greetingsHide();
+		}, 100);
+	} else {
+		document.getElementById("txt-greetings").style.display="none";
+		greetingsDisplay='False';
+	}
+	
+}
